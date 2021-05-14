e755a548348cc6597588bb962e1e81962a829674
Franco <francofl1999@gmail.com>
2021-05-14 11:58:03 -0300

    more models fixed

--- db/migrations/00005_create_questions_table.rb
+++ db/migrations/00005_create_questions_table.rb
@@ -3,14 +3,14 @@ Sequel.migration do
       create_table(:questions) do
         primary_key   :id
         String        :name
-        String        :description,
-        Int           :number,
+        String        :description
+        Int           :number
         String        :type
-        DateTime      :created_at,   default: Sequel::CURRENT_TIMESTAMP
-        DateTime      :updated_at,   default: Sequel::CURRENT_TIMESTAMP
+        DateTime      :created_at   default: Sequel::CURRENT_TIMESTAMP
+        DateTime      :updated_at   default: Sequel::CURRENT_TIMESTAMP
       end
     end
-  
+
     down do
       drop_table(:questions)
     end
