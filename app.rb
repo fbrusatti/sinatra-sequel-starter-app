d782c0cbd8ed1ab0f33cf2e06606a809444c8d1c
Franco <francofl1999@gmail.com>
2021-06-09 23:22:26 -0300

    finish survey

--- app.rb
+++ app.rb
@@ -5,39 +5,26 @@ class App < Sinatra::Base
     erb :login_template
   end
 
-  get "/hello/:name" do
-   @name = params[:name]
-   erb :hello_template
+  get '/make_survey' do
+    @questions = Question.all
+
+    @survey = Survey.create(username: params[:username])
+
+    erb :survey_template
   end
 
-  get "/show_careers" do
-    @careers = Career.all
+  post "/finish_survey" do
+    choicesSelected = JSON.parse request.body.read
 
-    erb :careers_template
-  end
+    clearCache()
 
-  post "/create_career" do
-    career = Career.new(name:params[:name])
+    createResponsesFromChoices(choicesSelected)
 
-      if career.save
-        [201,{'location' => "/show_careers"}, 'CREATED']
-      else
-        [500,{}, 'Internal Server Error']
-      end
-  end
+    points = getPoints();
 
-  get "/finish_survey" do
-    choicesSelected = params[:choices]
+    results = getCareersWithPoints(points);
 
-    # clearCache()
-
-    # createResponsesFromChoices(choicesSelected)
-
-    # points = getPoints();
-
-    # results = getCareersWithPoints(points);
-
-    choicesSelected.to_s
+    results.to_json
   end
 
   def clearCache()
@@ -48,7 +35,7 @@ class App < Sinatra::Base
 
   def createResponsesFromChoices(choices)
     choices.each do |choice|
-      Response.create(choice_id: choice['choiceId'].to_i, question_id: choice['questionId'].to_i)
+      Response.create(choice_id: choice['choiceId'], question_id: choice['questionId'])
     end
   end
 
@@ -85,27 +72,9 @@ class App < Sinatra::Base
     return results
   end
 
-  get '/make_survey' do
-    @questions = Question.all
+  get '/show_survey' do
+    @results = JSON.parse params[:results]
 
-    @survey = Survey.create(username: params[:username])
-
-    erb :survey_template
-  end
-
-  post "/posts" do
-    request.body.rewind  # in case someone already read it
-    data = JSON.parse request.body.read
-    post = Post.new(description: data['desc'])
-    if post.save
-      [201, { 'Location' => "posts/#{post.id}" }, 'CREATED']
-    else
-      [500, {}, 'Internal Server Error']
-    end
-  end
-
-  get '/posts' do
-    p = Post.where(id: 1).last
-    p.description
+    erb :result_survey
   end
 end
